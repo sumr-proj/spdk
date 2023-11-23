725f9bdf485cdfdbfe072bcc01c7aeba2b374e7e
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-11-23 14:49:03 +0000

    feat: Change read request logic.

--- module/bdev/raid/raid5.c
+++ module/bdev/raid/raid5.c
@@ -1170,6 +1170,77 @@ raid5_read_except_one_req_strip(struct raid5_stripe_request *request)
 	}
 }
 
+static void
+raid5_submit_read_request(struct raid5_stripe_request *request)
+{
+	struct raid_bdev_io 		*raid_io = request->raid_io;
+	struct spdk_bdev_io			*bdev_io = spdk_bdev_io_from_ctx(raid_io);
+	struct raid_bdev			*raid_bdev = raid_io->raid_bdev;
+	struct raid_bdev_io_channel	*raid_ch = raid_io->raid_ch;
+	struct spdk_io_channel		*base_ch;
+	uint64_t			ststrip_idx = raid5_start_strip_idx(bdev_io, raid_bdev);
+	uint64_t			estrip_idx = raid5_end_strip_idx(bdev_io, raid_bdev);
+	uint8_t				after_estrip_idx = raid5_next_idx(estrip_idx, raid_bdev);
+	int				ret = 0;
+
+	SPDK_ERRLOG("raid5_submit_read_request");
+
+	for (uint8_t idx = ststrip_idx; idx != after_estrip_idx; idx = raid5_next_idx(idx, raid_bdev)) {
+		base_ch = raid_ch->base_channel[idx];
+
+		if (base_ch == NULL) {
+			if (request->broken_strip_idx == raid_bdev->num_base_bdevs) {
+				request->broken_strip_idx = idx;
+			} else {
+				SPDK_ERRLOG("RAID5 read request: 2 broken strips\n");
+				raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
+				raid5_stripe_req_complete(request);
+				assert(false);
+				return;
+			}
+		}
+	}
+
+	if (request->broken_strip_idx == raid_bdev->num_base_bdevs) {
+		ret = raid5_read_req_strips_set_strip_buffs(request);
+		if (spdk_unlikely(ret != 0)) {
+			SPDK_ERRLOG("RAID5 read request: allocation of buffers is failed\n");
+			raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
+			raid5_stripe_req_complete(request);
+			return;
+		}
+
+		raid_io->base_bdev_io_submitted = 0;
+		raid_io->base_bdev_io_remaining = ((estrip_idx + raid_bdev->num_base_bdevs) -
+				ststrip_idx) % raid_bdev->num_base_bdevs + 1;
+		raid5_read_req_strips(request);
+	} else {
+		for (uint8_t idx = after_estrip_idx; idx != ststrip_idx; idx = raid5_next_idx(idx, raid_bdev)) {
+			base_ch = raid_ch->base_channel[idx];
+
+			if (base_ch == NULL) {
+				SPDK_ERRLOG("RAID5 read request: 2 broken strips\n");
+				raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
+				raid5_stripe_req_complete(request);
+				assert(false);
+				return;
+			}
+		}
+
+		ret = raid5_read_exc_req_strip_set_strip_buffs(request);
+		if (spdk_unlikely(ret != 0)) {
+			SPDK_ERRLOG("RAID5 read request: allocation of buffers is failed\n");
+			raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
+			raid5_stripe_req_complete(request);
+			return;
+		}
+
+		raid_io->base_bdev_io_submitted = 0;
+		raid_io->base_bdev_io_remaining = raid_bdev->num_base_bdevs - 1;
+		raid5_read_except_one_req_strip(request);
+	}
+}
+
 static void raid5_submit_rw_request(struct raid_bdev_io *raid_io);
 
 static void
@@ -1178,145 +1249,6 @@ _raid5_submit_rw_request(void *_raid_io)
 	struct raid_bdev_io *raid_io = _raid_io;
 
 	raid5_submit_rw_request(raid_io);
-}
-
-static void
-raid5_submit_read_request(struct raid_bdev_io *raid_io)
-{
-	struct spdk_bdev_io		*bdev_io = spdk_bdev_io_from_ctx(raid_io);
-	struct spdk_bdev_ext_io_opts	io_opts = {};
-	struct raid_bdev_io_channel	*raid_ch = raid_io->raid_ch;
-	struct raid_bdev		*raid_bdev = raid_io->raid_bdev;
-	uint64_t			block_size_b = (raid_bdev->strip_size_kb / raid_bdev->strip_size) * (uint64_t)1024;
-	uint64_t			stripe_index;
-	uint64_t			parity_strip_idx;
-	uint64_t			req_bdev_idx;
-	uint32_t			offset_in_strip;
-	uint64_t			offset_blocks;
-	uint64_t			num_blocks;
-	int				ret = 0;
-	uint64_t			start_strip_idx;
-	uint64_t			end_strip_idx;
-	struct raid_base_bdev_info	*base_info;
-	struct spdk_io_channel		*base_ch;
-
-	start_strip_idx = bdev_io->u.bdev.offset_blocks >> raid_bdev->strip_size_shift;
-	end_strip_idx = (bdev_io->u.bdev.offset_blocks + bdev_io->u.bdev.num_blocks - 1) >>
-		    raid_bdev->strip_size_shift;
-	if (start_strip_idx != end_strip_idx) {
-		SPDK_ERRLOG("I/O spans strip boundary!\n");
-		assert(false);
-		raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
-		return;
-	}
-
-	assert(raid_ch != NULL);
-	assert(raid_ch->base_channel);
-
-	io_opts.size = sizeof(io_opts);
-	io_opts.memory_domain = bdev_io->u.bdev.memory_domain;
-	io_opts.memory_domain_ctx = bdev_io->u.bdev.memory_domain_ctx;
-	io_opts.metadata = bdev_io->u.bdev.md_buf;
-
-	stripe_index = start_strip_idx / (raid_bdev->num_base_bdevs - 1);
-	parity_strip_idx = raid5_parity_strip_index(raid_bdev, stripe_index);
-	offset_in_strip = bdev_io->u.bdev.offset_blocks % (raid_bdev->strip_size);
-
-	req_bdev_idx = start_strip_idx % (raid_bdev->num_base_bdevs - 1);
-	if (req_bdev_idx >= parity_strip_idx) {
-		++req_bdev_idx;
-	}
-	offset_blocks = (stripe_index << raid_bdev->strip_size_shift) + offset_in_strip;
-	num_blocks = bdev_io->u.bdev.num_blocks;
-
-	base_info = &raid_bdev->base_bdev_info[req_bdev_idx];
-	base_ch = raid_ch->base_channel[req_bdev_idx];
-
-	if (base_ch != NULL) {
-		// case: reading only one strip
-
-		ret = spdk_bdev_readv_blocks_ext(base_info->desc, base_ch,
-						 bdev_io->u.bdev.iovs, bdev_io->u.bdev.iovcnt,
-						 offset_blocks, num_blocks, raid5_bdev_io_completion,
-						 raid_io, &io_opts);
-
-		if (ret == -ENOMEM) {
-			raid_bdev_queue_io_wait(raid_io, spdk_bdev_desc_get_bdev(base_info->desc),
-					base_ch, _raid5_submit_rw_request);
-		} else if (ret != 0) {
-			SPDK_ERRLOG("bdev io submit error not due to ENOMEM, it should not happen\n");
-			assert(false);
-			raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
-		}
-	} else {
-		// case: broken request strip
-
-		uint8_t start_idx;
-
-		if (raid_io->base_bdev_io_submitted == 0) {
-			raid_io->base_bdev_io_remaining = raid_bdev->num_base_bdevs - 1;
-			raid5_fill_iovs_with_zeroes(bdev_io->u.bdev.iovs, bdev_io->u.bdev.iovcnt);
-		}
-
-		start_idx = raid_io->base_bdev_io_submitted;
-		if (req_bdev_idx <= start_idx) {
-			start_idx++;
-		}
-
-		for (uint8_t idx = start_idx; idx < raid_bdev->num_base_bdevs; ++idx) {
-			struct raid5_io_buffer *io_buffer;
-
-			base_info = &raid_bdev->base_bdev_info[idx];
-			base_ch = raid_ch->base_channel[idx];
-			if (base_ch == NULL) {
-				if (idx == req_bdev_idx) {
-					continue;
-				} else {
-					SPDK_ERRLOG("2 broken strips\n");
-					assert(false);
-					raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
-					raid_io->base_bdev_io_remaining = raid_io->base_bdev_io_remaining + raid_io->base_bdev_io_submitted -
-															(raid_bdev->num_base_bdevs - 1);
-					if (raid_io->base_bdev_io_remaining == 0) {
-						raid_bdev_io_complete(raid_io, raid_io->base_bdev_io_status);
-					}
-					return;
-				}
-			}
-
-			io_buffer = raid5_get_io_buffer(raid_io, num_blocks * block_size_b);
-			if (io_buffer == NULL) {
-				raid_bdev_queue_io_wait(raid_io, spdk_bdev_desc_get_bdev(base_info->desc),
-					base_ch, _raid5_submit_rw_request);
-				return;
-			}
-			
-			ret = spdk_bdev_readv_blocks_ext(base_info->desc, base_ch,
-						 io_buffer->buffer, 1,
-						 offset_blocks, num_blocks, raid5_read_request_complete_part,
-						 io_buffer, &io_opts);
-
-			if (ret != 0) {
-				raid5_free_io_buffer(io_buffer);
-				if (ret == -ENOMEM) {
-					raid_bdev_queue_io_wait(raid_io, spdk_bdev_desc_get_bdev(base_info->desc),
-							base_ch, _raid5_submit_rw_request);
-				} else {
-					SPDK_ERRLOG("bdev io submit error not due to ENOMEM, it should not happen\n");
-					assert(false);
-					raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
-					raid_io->base_bdev_io_remaining = raid_io->base_bdev_io_remaining + raid_io->base_bdev_io_submitted -
-															(raid_bdev->num_base_bdevs - 1);
-					if (raid_io->base_bdev_io_remaining == 0) {
-						raid_bdev_io_complete(raid_io, raid_io->base_bdev_io_status);
-					}
-				}
-				return;
-			}			 
-
-			raid_io->base_bdev_io_submitted++;
-		}
-	}
 }
 
 static void raid5_submit_write_request_reading(struct raid5_io_buffer *wr_xor_buff);
@@ -1736,11 +1668,67 @@ raid5_submit_write_request(struct raid_bdev_io *raid_io)
 static void
 raid5_submit_rw_request(struct raid_bdev_io *raid_io)
 {
-	struct spdk_bdev_io		*bdev_io = spdk_bdev_io_from_ctx(raid_io);
+	struct spdk_bdev_io			*bdev_io = spdk_bdev_io_from_ctx(raid_io);
+	struct raid_bdev			*raid_bdev = raid_io->raid_bdev;
+	struct raid_bdev_io_channel	*raid_ch = raid_io->raid_ch;
+	struct raid5_info			*r5_info = raid_bdev->module_private;
+	struct raid5_stripe_request	*request;
+
+	// debug logs
+
+	SPDK_ERRLOG("iovcnt: %d\n", bdev_io->u.bdev.iovcnt);
+
+	for (int i = 0; i < bdev_io->u.bdev.iovcnt; ++i) {
+		SPDK_ERRLOG("iovec #%d, addr:%llu, len: %lu\n",
+				i,
+				bdev_io->u.bdev.iovs[i].iov_base,
+				bdev_io->u.bdev.iovs[i].iov_len);
+	}
+
+	uint64_t			start_strip;
+	uint64_t			end_strip;
+	uint64_t			ststrip_idx = raid5_start_strip_idx(bdev_io, raid_bdev);
+	uint64_t			estrip_idx = raid5_end_strip_idx(bdev_io, raid_bdev);
+	uint64_t			after_estrip_idx = raid5_next_idx(estrip_idx, raid_bdev);
+
+	start_strip = bdev_io->u.bdev.offset_blocks >> raid_bdev->strip_size_shift;
+	end_strip = (bdev_io->u.bdev.offset_blocks + bdev_io->u.bdev.num_blocks - 1) >>
+			raid_bdev->strip_size_shift;
+
+	SPDK_ERRLOG("start_strip: %lu\nend_strip: %lu\nststrip_idx: %lu\nestrip_idx: %lu\nafter_estrip_idx: %lu\n",
+			start_strip,
+			end_strip,
+			ststrip_idx,
+			estrip_idx,
+			after_estrip_idx);
+
+	// end debug logs
+
+	if (!raid5_check_io_boundaries(raid_io)) {
+		SPDK_ERRLOG("RAID5: I/O spans stripe boundaries!\n");
+		raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
+		assert(false);
+		return;
+	}
+
+	raid5_check_raid_ch(raid_ch);
+
+	if (bdev_io->type == SPDK_BDEV_IO_TYPE_WRITE && r5_info->write_type == UNDEFINED) {
+		SPDK_ERRLOG("RAID5: write type is undefinied\n");
+		raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
+		return;
+	}
+
+	request = raid5_get_stripe_request(raid_io);
+	if (request == NULL) {
+		SPDK_ERRLOG("RAID5: allocation of stripe request is failed\n");
+		raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
+		return;
+	}
 
 	switch (bdev_io->type) {
 	case SPDK_BDEV_IO_TYPE_READ:
-		raid5_submit_read_request(raid_io);
+		raid5_submit_read_request(request);
 		break;
 	case SPDK_BDEV_IO_TYPE_WRITE:
 		raid5_submit_write_request(raid_io);
