3bb2b10010724fff06ac001700806e904744149f
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-12-15 03:02:29 +0000

    feat: Add cb and part completion fns for read-modify-write reading part.

--- module/bdev/raid/raid5.c
+++ module/bdev/raid/raid5.c
@@ -2034,6 +2034,93 @@ raid5_write_r_modify_w_writing(struct raid5_stripe_request *request)
 	}
 }
 
+static bool
+raid5_w_r_modify_w_reading_complete_part(struct raid5_stripe_request *request, uint64_t completed,
+				enum spdk_bdev_io_status status)
+{
+	struct raid_bdev_io *raid_io = request->raid_io;
+
+	SPDK_ERRLOG("raid5_w_r_modify_w_reading_complete_part\n");
+
+	assert(raid_io->base_bdev_io_remaining >= completed);
+	raid_io->base_bdev_io_remaining -= completed;
+
+	if (status != SPDK_BDEV_IO_STATUS_SUCCESS) {
+		raid_io->base_bdev_io_status = status;
+	}
+
+	if (raid_io->base_bdev_io_remaining == 0) {
+		struct raid_bdev 		*raid_bdev = raid_io->raid_bdev;
+		struct spdk_bdev_io		*bdev_io = spdk_bdev_io_from_ctx(raid_io);
+		uint64_t block_size_b8 = ((uint64_t)128 * raid_bdev->strip_size_kb) /
+					raid_bdev->strip_size;
+		uint8_t sts_idx = raid5_start_strip_idx(bdev_io, raid_bdev);
+		uint8_t es_idx = raid5_end_strip_idx(bdev_io, raid_bdev);
+		uint8_t after_es_idx = raid5_next_idx(es_idx, raid_bdev);
+		uint8_t ps_idx = raid5_parity_strip_index(raid_bdev, raid5_stripe_idx(bdev_io, raid_bdev));
+		uint64_t ofs_b8;
+		uint64_t num_b8;
+		int ret = 0;
+
+		if (raid_io->base_bdev_io_status != SPDK_BDEV_IO_STATUS_SUCCESS) {
+			raid5_w_r_modify_w_reading_free_strip_buffs(request);
+			raid5_stripe_req_complete(request);
+			return true;
+		}
+
+		for (uint8_t i = sts_idx; i != after_es_idx; i = raid5_next_idx(i, raid_bdev)) {
+			ofs_b8 = (raid5_ofs_blcks(bdev_io, raid_bdev, i) -
+					raid5_ofs_blcks(bdev_io, raid_bdev, es_idx)) * block_size_b8;
+			num_b8 = raid5_num_blcks(bdev_io, raid_bdev, i) * block_size_b8;
+			raid5_xor_iovs_with_iovs(request->strip_buffs[ps_idx],
+					request->strip_buffs_cnts[ps_idx], ofs_b8,
+					request->strip_buffs[i], request->strip_buffs_cnts[i], 0,
+					num_b8);
+		}
+
+		ret = raid5_write_r_modify_w_reset_strip_buffs(request);
+		if (spdk_unlikely(ret != 0)) {
+			SPDK_ERRLOG("RAID5 write request: allocation of buffers is failed\n");
+			raid_io->base_bdev_io_status = SPDK_BDEV_IO_STATUS_FAILED;
+			raid5_stripe_req_complete(request);
+			return true;
+		}
+
+		for (uint8_t i = sts_idx; i != after_es_idx; i = raid5_next_idx(i, raid_bdev)) {
+			ofs_b8 = (raid5_ofs_blcks(bdev_io, raid_bdev, i) -
+					raid5_ofs_blcks(bdev_io, raid_bdev, es_idx)) * block_size_b8;
+			num_b8 = raid5_num_blcks(bdev_io, raid_bdev, i) * block_size_b8;
+			raid5_xor_iovs_with_iovs(request->strip_buffs[ps_idx],
+					request->strip_buffs_cnts[ps_idx], ofs_b8,
+					request->strip_buffs[i], request->strip_buffs_cnts[i], 0,
+					num_b8);
+		}
+
+		raid_io->base_bdev_io_submitted = 0;
+		raid_io->base_bdev_io_remaining = (((es_idx + raid_bdev->num_base_bdevs) -
+					sts_idx) % raid_bdev->num_base_bdevs) + 2;
+
+		raid5_write_r_modify_w_writing(request);
+
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static void
+raid5_w_r_modify_w_reading_cb(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg) {
+	struct raid5_stripe_request *request = cb_arg;
+
+	SPDK_ERRLOG("raid5_w_r_modify_w_reading_cb\n");
+
+	spdk_bdev_free_io(bdev_io);
+
+	raid5_w_r_modify_w_reading_complete_part(request, 1, success ?
+					SPDK_BDEV_IO_STATUS_SUCCESS :
+					SPDK_BDEV_IO_STATUS_FAILED);
+}
+
 static void raid5_submit_rw_request(struct raid_bdev_io *raid_io);
 
 static void
